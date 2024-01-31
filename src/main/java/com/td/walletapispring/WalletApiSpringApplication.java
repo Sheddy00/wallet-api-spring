8646da033ca6c90e2d9806efb87b458be6164e54
Sheddy00 <hei.sheddy.2@gmail.com>
2024-01-31 20:43:05 +0100

    first commit

---
+++ src/main/java/com/td/walletapispring/WalletApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.td.walletapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/td/walletapispring/WalletApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.walletapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
